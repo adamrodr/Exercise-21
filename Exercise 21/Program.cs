3e0a4dca65c53894a56466a7ca2539967c75cad5
moi <moi@DESKTOP-DNHOSUB>
2023-10-03 18:12:01 +0200

    Ajoutez des fichiers projet.

---
+++ Exercise 21/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int note, upmoy,downmoy,k;
+            note = 0;
+            k = 0;
+            upmoy = 0;
+            downmoy = 0;
+            Console.WriteLine("entrez vos notes");
+
+            for (k = 0; k <=10 ; k++) 
+            {
+
+                note = int.Parse(Console.ReadLine());
+
+                if (note > 11)
+                {
+                    upmoy++;
+
+                }
+                else
+                {
+                   downmoy++;
+                }
+                
+            }
+
+            Console.WriteLine(upmoy + " eleves sont au dessus de 10 et" + downmoy + " eleves sont en dessous de 10!");
+            Console.ReadLine();
+
+        }
+    }
+}
